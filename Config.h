9a700e63841f5a1d525386dfc6cd054895c4940b
Mike Petrichenko <mike@btframework.com>
2025-10-03 14:54:45 +0300

    Update Config.h

--- Config.h
+++ Config.h
@@ -1,7 +1,5 @@
 #pragma once
 
-//#include "PrivateConfig.h"
-
 /*******************************************************************************/
 /*                         Temperature sensor settings                         */
 
